f5765aa0a9d70bf97dd1ab192fac00658331c664
Temi <tayolanre2000@yahoo.com>
2021-05-23 17:07:39 +0100

    UPDATE: Added find one product

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -1,2 +1,20 @@
 const router = require('express').Router();
 const { Product, Category, Tag, ProductTag } = require('../../models');
+
+const findProduct = (_id) => 
+  Product.findOne({
+      include:[{model: Tag, as: 'tags', required: false}, {model: Category, as: 'category', required: false}],
+      where: {id: _id}
+});
+
+const okMessage = {    
+  message: "ok"
+};
+
+// get one product
+router.get('/:id', (req, res) => {
+    // find a single product by its `id`
+    // be sure to include its associated Category and Tag data
+    findProduct(req.params.id).then((product) => res.json(product));
+});
+  
