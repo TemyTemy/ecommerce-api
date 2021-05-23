55b7b0a96fc7c2add8b520a712ee8555498d230c
Temi <tayolanre2000@yahoo.com>
2021-05-23 17:15:34 +0100

    UPDATE: Added find all tags

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -11,5 +11,13 @@ const okMessage = {
     message: "ok"
 };
 
+// The `/api/tags` endpoint
+
+router.get('/', (req, res) => {
+  // find all tags
+  // be sure to include its associated Product data
+    Tag.findAll({include:[{model: Product, as: 'tag_products'}]}).then((products) => res.json(products));
+});
+
 
 module.exports = router;
